8acf6cbad0d05de5a844d54d1dd57b0d572496f2
aljaneb alkhafi <business.aljanibkhafi@gmail.com>
2026-01-17 17:05:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9883590042-3e8f8",
+  "appId": "1:960293145836:web:b035508be762212a0a29c9",
+  "apiKey": "AIzaSyDY3qDad3SPDn6lLlSEUY7fAxH-GGObOjk",
+  "authDomain": "studio-9883590042-3e8f8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "960293145836"
+};
